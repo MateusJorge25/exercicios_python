a95f94dc0b0fa1f8d200cc3af0e9cac22b0ce608
Mateusj2506 <mateus_jorge2002@outlook.com>
2023-10-27 20:59:59 -0300

    exercicios

---
+++ graus_celcius.py
@@ -0,0 +1,6 @@
+
+f = int(input("Digite os graus em fahrenheit:"))
+
+c = 5 * ((f-32)/9)
+
+print("A temperatura em graus Celcius é:", c)
---
+++ salario.py
@@ -0,0 +1,9 @@
+
+ganho_hora= float(input("Digite quanto vc ganha por hora:"))
+
+
+horas_trab= int(input("Digite o numero de horas trabalhadas:"))
+
+resultado = ganho_hora * horas_trab
+
+print("Seu salario no final do mês é de:", resultado)
